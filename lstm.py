03c46bf673c65fd01e5841541e264565ddadfa63
antoniocsj <antoniocsj@outlook.com>
2020-07-18 18:41:52 -0300

    fixed some imports.

--- lstm.py
+++ lstm.py
@@ -1,22 +1,25 @@
 import sys
-from keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 from utils import getDataset, defineModel, getText
 
+
 def train(filename):
-  raw_text = getText(filename)
-  data = getDataset(raw_text)
-  model = defineModel(data)
-  fitModel(model, data)
+    raw_text = getText(filename)
+    data = getDataset(raw_text)
+    model = defineModel(data)
+    fitModel(model, data)
+
 
 def fitModel(model, data):
-  filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
-  checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
-  callbacks_list = [checkpoint]
+    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
 
-  # fit the model
-  model.fit(data['input'], data['output'], nb_epoch=200, batch_size=64, callbacks=callbacks_list)
-    
+    # fit the model
+    model.fit(data['input'], data['output'], epochs=200, batch_size=64, callbacks=callbacks_list)
+
+
 if __name__ == "__main__":
-    filename = sys.argv[1];
+    filename = sys.argv[1]
     train(filename)
